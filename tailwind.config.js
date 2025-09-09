b7c188ab7e7519288b695ea0c285d4ee98f337af
kat-log <greentea.tomatosystem@gmail.com>
2025-09-10 08:20:56 +0900

    update: package.jsonにモジュールタイプを追加し、tailwind.config.jsをESM形式に修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,3 +17,5 @@ export default {
   },
   plugins: [typography],
 };
+
+export default config;
